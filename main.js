058f560454ccfbbcbb12a9628fe1c5ebfca6965a
unknown <wahyuchaosingan@gmail.com>
2020-04-18 16:26:50 +0800

    Project UTS STMIK Primakara

--- main.js
+++ main.js
@@ -1,41 +1,117 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const electron = require("electron");
+const uuid = require("uuid").v4;
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+const {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain
+} = electron;
+
+let indexWindow;
+let createWindow;
+let listWindow;
+
+let allShopping = [];
+
+app.on("ready", () => {
+    indexWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: "Chess Shop"
+    });
+
+    indexWindow.loadURL(`file://${__dirname}/index.html`);
+    indexWindow.on("closed", () => {
+
+        app.quit()
+        indexWindow = null;
+
+    });
+        const mainMenu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(mainMenu);
+    });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Product List"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Completely Your Shopping"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+
+
+};
+
+ipcMain.on("shopping:create", (event, shopping) => {
+    shopping["id"] = uuid();
+    shopping["done"] = 0;
+    allShopping.push(shopping);
+
+    createWindow.close();
+
+    console.log(allShopping);
+});
+ipcMain.on("shopping:request:list", event => {
+    listWindow.webContents.send('shopping:response:list', allShopping);
+});
+ipcMain.on("shopping:done", (event, id) => {
+    console.log("here");
+});
+
+const menuTemplate = [{
+        label: "file",
+        submenu: [{
+                label: "New Product",
+
+                click() {
+                    createWindowCreator();
+                }
+            },
+            {
+                label: "List Product",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command+Q" : "CTRL + Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+
+    {
+        label: "View",
+        submenu: [{
+            role: "reload"
+        }, {
+            role: "toggledevtools"
+        }]
     }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+]
